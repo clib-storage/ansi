57b4db5641c9b23cfc38837aaac716f254a70258
T0ine34 <91411073+T0ine34@users.noreply.github.com>
2023-03-27 22:30:50 +0200

    adding comments

--- ansi.h
+++ ansi.h
@@ -9,6 +9,10 @@
 #endif
 
 
+/**
+ * @brief Disable ANSI support, this function is called automatically when the program exits
+ * 
+ */
 void disableANSI(){
     #ifdef _WIN32
         SetConsoleMode(ANSI_storage, 0);
@@ -16,6 +20,10 @@ void disableANSI(){
     //linux have a native support for ANSI, so no need to do anything
 }
 
+/**
+ * @brief Enable ANSI support, you need to call this function before using any ANSI function
+ * 
+ */
 void enableANSI(){
     atexit(disableANSI);
     #ifdef _WIN32
